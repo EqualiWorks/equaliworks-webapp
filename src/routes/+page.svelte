388871f68e034ebdf6520d78f5d122aa215b6e83
Frederik Fink Højriis <frfh@itu.dk>
2023-02-03 22:18:33 +0100

    add navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/jobs">jobs</a>
+            <a href="/companies">companies</a>
+            <a href="/about">about</a>
+        </li>
+    </ul>
+</nav>
+
 <div class="h-screen w-screen flex justify-center items-center font-mono">
     EqualiWorks
 </div>
