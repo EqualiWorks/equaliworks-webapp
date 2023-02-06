6713a40f21b5fc2523bf36042eefebd536d63a1d
Frederik Fink Højriis <frfh@itu.dk>
2023-02-06 15:25:32 +0100

    add user info to profile

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { enhance, type SubmitFunction } from '$app/forms';
+	import { page } from '$app/stores';
 	import Button from '$lib/components/form/+Button.svelte';
 
 	const submitLogout: SubmitFunction = async ({ cancel }) => {};
 </script>
 
 <div class="p-4">
+	<div>
+		<h2 class="font-display">{$page.data.session?.user.user_metadata.name}</h2>
+		<h2 class="font-mono">{$page.data.session?.user.user_metadata.email}</h2>
+	</div>
 	<form action="?/sign-out" method="POST" use:enhance={submitLogout}>
 		<Button class="hover:text-red-500">sign out</Button>
 	</form>
