12bfce25deb4e52a03482a1676dcaaa27f76bc4b
Frederik Fink Højriis <frfh@itu.dk>
2023-02-07 22:16:39 +0100

    Update +InputEmail.svelte

--- src/lib/components/form/+InputEmail.svelte
+++ src/lib/components/form/+InputEmail.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="mb-6">
-	<label for={id} class="block mb-1 text-xs font-mono text-neutral-700 dark:text-white capitalize">
+	<label for={id} class="mb-1 block font-mono text-xs capitalize text-neutral-700 dark:text-white">
 		{label}
 	</label>
 	<input
@@ -18,7 +18,7 @@
 		bind:value
 		autocomplete="email"
 		type="email"
-		class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-800 dark:border-neutral-700 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+		class="block w-full rounded border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-neutral-700 dark:bg-neutral-800 dark:text-white dark:placeholder-neutral-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 		{placeholder}
 		{required}
 	/>
