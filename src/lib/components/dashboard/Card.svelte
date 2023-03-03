3073c3e1fcefe07442ff897051b5f7af2a0d1f50
chtof <chtof@itu.dk>
2023-03-03 19:54:53 +0100

    feature: Initial card creation

---
+++ src/lib/components/dashboard/Card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    //export let data: {
+    export let title: string;
+    export let    icon: string;
+    export let    value: number;
+    export let    change: string;
+    export let    timeframe: string;
+    //};
+</script>
+
+<div class="box-border relative w-1/6 p-5 group bg-gray-200 dark:bg-slate-600 dark:text-white hover:bg-green-400 rounded-md opacity-75 hover:opacity-100">
+    <span class="animate-ping absolute inline-flex h-3 w-3 right-0 top-0 rounded-full bg-red-600 "></span>
+    <div class="flex">
+        <p>{title}</p>
+        <i class="{icon} ml-auto"></i>
+    </div>
+    <div class="mt-5">
+        <h2>{value}</h2>
+    </div>
+    <div class="flex mt-2 box-border">
+        <p class="px-1 rounded-md bg-gray-300 dark:bg-slate-700 group-hover:bg-emerald-400">+{change} </p>
+        <p>{timeframe}</p>
+    </div>
+</div>
--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,4 +1,8 @@
-<div class="m-4">
+<script lang=ts>
+	import Card from "$lib/components/dashboard/Card.svelte";
+
+</script>
+<div class="m-4 space-y-20">
 	<h1 class="font-display dark:text-white">
 		Start your hiring project in a <span
 			class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-green-400"
@@ -6,4 +10,12 @@
 		>
 		manner.
 	</h1>
+	<div class="font-display dark:text-white">
+		<h2>Your stats</h2>
+	</div>
+	<div class="justify-center flex space-x-6">
+		<Card title={"Sent job applications this week"} icon={"ph-paper-plane-tilt"} value={5} change={"2"} timeframe={"more than last week"}/>
+		<Card title={"Matching job offers"} icon={"ph-percent"} value={2} change={"1"} timeframe={"new this week"}/>
+		<Card title={"Bing chilling"} icon={"ph-cube"} value={15} change={"5%"} timeframe={"brrrrrr"}/>
+	</div>
 </div>
